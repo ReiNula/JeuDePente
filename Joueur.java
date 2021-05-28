03a38f38a5f75315e4a8f2d0221eca02bb3e2c60
Laurine <leulliette.laurine@gmail.com>
2021-05-28 10:59:26 +0200

    Change player color

--- Joueur.java
+++ Joueur.java
@@ -15,7 +15,7 @@ public class Joueur {
     // Constructeur par défaut
     public Joueur() {
         this.name = "Joueur";
-        this.color = "NOIR";
+        this.color = "BLANC";
         this.active = false;
         this.win =false;
         this.pairCaptured = 0;
